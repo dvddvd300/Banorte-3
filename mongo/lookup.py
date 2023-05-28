cfb4e23e9e8ee9a78eb56e4cfa6647b456ca3441
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-05-28 11:33:18 -0600

    Update lookup

--- mongo/lookup.py
+++ mongo/lookup.py
@@ -1,22 +1,3 @@
 import os
 
 os.system('node mongo/mongo_connection.js')
-""" 
-
-uri = "mongodb+srv://juanpgtzg:ljIgvpQJzlYZJwfF@webdata.w6ffrza.mongodb.net/?retryWrites=true&w=majority"
-
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-
-while True:
-    documents = client.get_database("machine_learning").get_collection("json_files").find()
-
-    # Convert documents to a list
-    document_list = list(documents)
-
-    # Save documents to a JSON file
-    with open("output.json", "w") as file:
-        json.dump(document_list, file)
-
-    time.sleep(10)
-client.close() """
